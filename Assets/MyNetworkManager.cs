73ffb06aac7ff12c9953079482c51151a4ebf8de
david carlson <dcarl661@gmail.com>
2016-10-17 18:42:59 -0700

    added MyNetworkManager and rehooked up the player prefab

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+
+    public void MyStartHost()
+    {
+        Debug.Log("MyNetworkManager.MyStartHost at " + Time.timeSinceLevelLoad);
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log("MyNetworkManager.OnStartHost at " + Time.timeSinceLevelLoad);
+    }
+
+}
