406534ad485e7abd9d3ce0fd1bb610f861965514
david carlson <dcarl661@gmail.com>
2016-10-17 20:51:47 -0700

    added onclientconnect

--- Assets/MyNetworkManager.cs
+++ Assets/MyNetworkManager.cs
@@ -16,4 +16,20 @@ public class MyNetworkManager : NetworkManager {
         Debug.Log("MyNetworkManager.OnStartHost at " + Time.timeSinceLevelLoad);
     }
 
+    public override void OnStartClient(NetworkClient client)
+    {
+        Debug.Log("MyNetworkManager.OnStartClient at " + Time.timeSinceLevelLoad);
+        InvokeRepeating("PrintDots", 0f, 1f);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log("MyNetworkManager.OnClientConnect at " + Time.timeSinceLevelLoad + " address="+conn.address);
+        CancelInvoke();
+    }
+
+    void PrintDots()
+    {
+        Debug.Log(".");
+    }
 }
