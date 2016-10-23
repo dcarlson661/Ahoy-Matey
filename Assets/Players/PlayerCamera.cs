2c33beca720466f83668a5c63e87dc0aa8df13b0
david carlson <dcarl661@gmail.com>
2016-10-23 12:25:10 -0700

    add camera rotation

---
+++ Assets/Players/PlayerCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+    //http://gamedev.stackexchange.com/questions/104693/how-to-use-input-getaxismouse-x-y-to-rotate-the-camera
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("PlayerCameraStart ");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+    }
+}
